7c7ac9fa1357872057c5c47b38e33d0273bf5c26
Tapaswee2001Samantaray <tapas10231@gmail.com>
2023-02-15 23:22:22 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,82 +1,24 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState , useEffect} from "react";
+import "./App.css"
 
-export default function App() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [gender, setGender] = useState('');
-  const [country, setCountry] = useState('');
-  const [marketing, setMarketing] = useState(false);
-  
+const JsonPlaceHoler = "https://jsonplaceholder.typicode.com/posts"
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+const JsonPlaceHolerList = () => {
+    const [jsonList,setJsonList]=useState([])
 
-    if (firstName && lastName && gender && country && marketing) {
-      console.log(firstName);
-      console.log(lastName);
-      console.log(gender);
-      console.log(country);
-      console.log(marketing);
-    } else {
-      alert('Please enter all fields');
-    }
-  };
-  return (
-    <div>
-      <h1>User Registration Form</h1>
-      <form onSubmit={handleSubmit}>
-        <section>
-          <label>First Name</label>
-          <input
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </section>
-        <section>
-          <label>Last Name</label>
-          <input
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </section>
-        <section>
-          <label>Gender</label>
-          <input
-            onChange={(e) => setGender(e.target.value)}
-            type="radio"
-            value="male"
-            checked={gender === 'male'}
-          />{' '}
-          Male
-          <input
-            onChange={(e) => setGender(e.target.value)}
-            type="radio"
-            value="female"
-            checked={gender === 'female'}
-          />{' '}
-          Female
-        </section>
-        <section>
-          <label>Country</label>
-          <select value={country} onChange={(e) => setCountry(e.target.value)}>
-            <option>Select an option...</option>
-            <option value="India">India</option>
-            <option value="USA">USA</option>
-            <option value="Australia">Australia</option>
-            <option value="UK">UK</option>
-          </select>
-        </section>
-        <section>
-          <label>Do you want to receive marketing emails?</label>
-          <input
-            type="checkbox"
-            value={marketing}
-            onChange={() => setMarketing(!marketing)}
-          />
-        </section>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    useEffect(()=>{
+        fetch(JsonPlaceHoler).then(response=>response.json()).then(data=>setJsonList(data))
+    },[])
+
+    return(
+        <div className="post">
+            {jsonList.map((post)=>(
+                <div key={post.id} className="post-card">
+                  <h2>{post.title}</h2>
+                  <p>{post.body}</p>
+                </div>
+            ))}
+        </div>
+    )
 }
+export default JsonPlaceHolerList;
